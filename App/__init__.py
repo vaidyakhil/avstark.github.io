5ef8f95a0873588282c107065e2d873169a69b06
avstark <vaidyakhil@gmail.comclear>
2020-07-14 00:12:41 +0530

    import statements

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from App.config import Config
+from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
